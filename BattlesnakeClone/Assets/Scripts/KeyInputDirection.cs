0c45bd0f0bd1ff2a9c229e7ce75622db15e25f32
Dimitrios Timoleon <mitsostim07@gmail.com>
2022-11-04 10:17:09 +0100

    Move with constant speed through tiles

---
+++ BattlesnakeClone/Assets/Scripts/KeyInputDirection.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInputDirection 
+{
+    public KeyCode code;
+    public int x;
+    public int y;
+}
--- BattlesnakeClone/Assets/Scripts/Snake.cs
+++ BattlesnakeClone/Assets/Scripts/Snake.cs
@@ -6,14 +6,20 @@ public class Snake : MonoBehaviour
 {
     private Vector2Int gridMoveDirection;
     private Vector2Int gridPosition;
-    private float gridMoveTimer;
-    public float gridMoveTimerMax;
+    private Rigidbody2D body;
+    private List<KeyInputDirection> keysAndDirections;
+    [SerializeField] private float moveSpeed = 1f;
 
     private void Awake()
     {
         gridPosition = new Vector2Int(0, 0);
-        gridMoveTimer = gridMoveTimerMax;
         gridMoveDirection = new Vector2Int(1, 0);
+        body = GetComponent<Rigidbody2D>();
+        keysAndDirections = new List<KeyInputDirection>();
+        keysAndDirections.Add(new KeyInputDirection { code = KeyCode.UpArrow, x = 0, y = 1 });
+        keysAndDirections.Add(new KeyInputDirection { code = KeyCode.DownArrow, x = 0, y = -1 });
+        keysAndDirections.Add(new KeyInputDirection { code = KeyCode.LeftArrow, x = -1, y = 0 });
+        keysAndDirections.Add(new KeyInputDirection { code = KeyCode.RightArrow, x = 1, y = 0 });
     }
 
     private void Update()
@@ -23,52 +29,36 @@ public class Snake : MonoBehaviour
     }
 
     private void HandleInput()
+    {        
+        CheckKeyInputs();
+    }
+
+    private void CheckKeyInputs()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        foreach (var keyInput in keysAndDirections)
         {
-            if (gridMoveDirection.y != -1)
+            if (Input.GetKeyDown(keyInput.code))
             {
-                gridMoveDirection.x = 0;
-                gridMoveDirection.y = 1;
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            if (gridMoveDirection.y != 1)
-            {
-                gridMoveDirection.x = 0;
-                gridMoveDirection.y = -1;
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            if (gridMoveDirection.x != 1)
-            {
-                gridMoveDirection.x = -1;
-                gridMoveDirection.y = 0;
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            if (gridMoveDirection.x != -1)
-            {
-                gridMoveDirection.x = 1;
-                gridMoveDirection.y = 0;
+                SetRoundedPosition();
+                if (gridMoveDirection.x != -keyInput.x && gridMoveDirection.y != -keyInput.y)
+                {
+                    gridMoveDirection.x = keyInput.x;
+                    gridMoveDirection.y = keyInput.y;
+                }
             }
         }
     }
 
-    private void HandleGridMovement()
+    private void SetRoundedPosition()
     {
-        gridMoveTimer += Time.deltaTime;
-        if (gridMoveTimer >= gridMoveTimerMax)
-        {
-            gridPosition += gridMoveDirection;
-            gridMoveTimer -= gridMoveTimerMax;
+        var roundedPosition = new Vector3(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
+        transform.position = roundedPosition;
+    }
 
-            transform.position = new Vector3(gridPosition.x, gridPosition.y);
-            transform.eulerAngles = new Vector3(0, 0, GetAngleFromVectorFloat(gridMoveDirection) - 90);
-        }        
+    private void HandleGridMovement()
+    {                      
+        body.velocity = new Vector2(gridMoveDirection.x, gridMoveDirection.y) * moveSpeed;
+        transform.eulerAngles = new Vector3(0, 0, GetAngleFromVectorFloat(gridMoveDirection) - 90);  
     }
 
     private float GetAngleFromVectorFloat(Vector2 dir)
